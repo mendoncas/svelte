76832b9fcb1c8d8e1740830522b71aef788c9c0d
Ricardo César <57590579+mendoncas@users.noreply.github.com>
2020-12-03 11:46:29 -0300

    Add files via upload

---
+++ save.js
@@ -0,0 +1,53 @@
+const getData = () => {
+    //chamar a API dqaqui, usar async await
+
+    let [dates, values, amounts] = [[], [], []];
+    /*.data;*/
+    axios.get("http://localhost:3000/deal/all").then((res) => {
+      for (const transaction of res.data) {
+        dates.push(transaction.date);
+        values.push(transaction.value);
+        amounts.push(transaction.amount);
+      }
+
+      let [back1, back2] = [[], []];
+      for (let i = 0; i < dates.length; i++) {
+        back1.push("rgba(196, 69, 54,0.9)");
+        back2.push("rgba(25, 114, 120,0.9)");
+      }
+
+      setChartData({
+        labels: dates,
+        datasets: [
+          {
+            label: "gasto",
+            data: values,
+            // opçoes de aparencia aqui
+            // rgb(25, 114, 120) azul
+            // rgb(237, 221, 212) beje
+            // rgb(196, 69, 54) vermelho
+            // rgb(119, 46, 37) vermelho escuro
+            // rgb(40, 61, 59) azul escuro
+            backgroundColor: back1,
+          },
+          {
+            label: "quantidade de ração comprada",
+            data: amounts,
+            backgroundColor: back2,
+          },
+        ],
+      });
+
+      console.log(chartData);
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <Bar data={chartData} />
+    </div>
+  );
